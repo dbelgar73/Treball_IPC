11d7747ada995d566202bbeb14844088ac3d2d42
david <>
2022-05-02 21:06:18 +0200

    Creada estructura vistes-controladors, afegit IniciSessio.fxml, Registre.fxml i controladors

---
+++ src/poiupv/controladors/IniciSesioController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poiupv.controladors;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author david
+ */
+public class IniciSesioController implements Initializable {
+
+    @FXML
+    private Label MissatgeUsuari;
+    @FXML
+    private Label MissatgeContrasenya;
+    @FXML
+    private TextField usuari;
+    @FXML
+    private Button iniciarSessio;
+    @FXML
+    private PasswordField contrassenya;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
