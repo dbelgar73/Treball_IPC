fd4a6860cb193a9d7ee2e4ffc112a1cf5e640079
david <>
2022-05-10 20:12:39 +0200

    Registre, completat la edat per a que tin ga en conte els mesos i els dies (inacabat per error en registre, falta registrar)

--- src/poiupv/controladors/RegistreController.java
+++ src/poiupv/controladors/RegistreController.java
@@ -117,20 +117,39 @@ public class RegistreController implements Initializable {
         int edat = 0;
         LocalDate birthDate = daypicker.getValue();
         if(birthDate != null){
+            //Obte la data de ara
+            int year, month, day = 0;
             LocalDate now = LocalDate.now();
-            int year = now.getYear();
-            edat = year - birthDate.getYear() ;
-            if(edat < 17){//es menor de 16 anys
-                errorEdat.setVisible(true);
+            //calcula la diferencia (Data de hui - any de naixement) = edat
+            int dataHui = now.getYear() + now.getDayOfYear();//calcula la data de hui
+            int naixement = birthDate.getYear() + birthDate.getDayOfYear();
+            edat = dataHui - naixement ;
+            if(edat < 16){//es menor de 16 anys
+                errorEdat.setVisible(true);//mostra error edat 
             }
             else{
-                errorEdat.setVisible(false);
+                errorEdat.setVisible(false);//amaga el error de la edat
             }
         }
-        if(User.checkNickName(usr) & User.checkEmail(email) & User.checkPassword(pswd) & edat > 16){
+        if(User.checkNickName(usr) && User.checkEmail(email) && User.checkPassword(pswd)){
             //TOT CORRECTE, REGISTRA
             navegacio.registerUser(usr,email,pswd,avatar,birthDate);
-            //VES A LA FINESTRA DE INICI DE SESIO
+            //VES A LA FINESTRA DE INICI DE SESIO I TANCA ESTA FINESTRA
+            try {
+            FXMLLoader miCargador = new FXMLLoader(getClass().getResource("/poiupv/vistes/Menu.fxml"));
+            Parent root = miCargador.load();
+            RegistreController controlador = miCargador.getController();
+            Scene scene = new Scene(root);
+            Stage estageActual = new Stage();
+            estageActual.setResizable(true);
+            estageActual.setScene(scene);
+            estageActual.initModality(Modality.APPLICATION_MODAL);
+            estageActual.show();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+            //TANCA LA FINESTRA DEL REGISTRE
             Node n = (Node)event.getSource();
             n.getScene().getWindow().hide();
         }
