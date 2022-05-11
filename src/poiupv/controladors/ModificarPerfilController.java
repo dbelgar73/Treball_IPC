a647969c47c873c1cf5cd0cbb321a247c8465011
Sergi <Sergi@DESKTOP-JHEVJ42.technicolor.net>
2022-05-11 19:31:32 +0200

    he creat el escenari per a registrarse He creat el escenari per a fer problemes al ficar tancar sesio te porta al escenari de inici de sesio escenari menu creat

--- src/poiupv/controladors/ModificarPerfilController.java
+++ src/poiupv/controladors/ModificarPerfilController.java
@@ -7,6 +7,8 @@ package poiupv.controladors;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -23,5 +25,13 @@ public class ModificarPerfilController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void guardarCambios(ActionEvent event) {
+    }
+
+    @FXML
+    private void cancelar(ActionEvent event) {
+    }
     
 }
