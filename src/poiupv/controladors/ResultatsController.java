eb541331dea62f6555b2dc630c145ef03a072a15
Usuario <Usuario@DESKTOP-7QGDEHR>
2022-05-14 13:32:27 +0200

    edicio

--- src/poiupv/controladors/ResultatsController.java
+++ src/poiupv/controladors/ResultatsController.java
@@ -6,8 +6,18 @@
 package poiupv.controladors;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import model.Answer;
+import model.Session;
+import poiupv.Poi;
 
 /**
  * FXML Controller class
@@ -16,12 +26,45 @@ import javafx.fxml.Initializable;
  */
 public class ResultatsController implements Initializable {
 
+    @FXML
+    private Label encerts;
+    @FXML
+    private Label fallades;
+    @FXML
+    private Label sesio;
+    @FXML
+    private ListView<Session> listSesions;
+    public List<Session> llistaSesions;
+    private ObservableList<Session> datos = null;
+    private int hits;
+    private int faults;
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        String data = null;
+        llistaSesions = (Poi.userActual).getSessions();
+        
+        datos = FXCollections.observableList(llistaSesions);
+        listSesions.setItems(datos);
+        listSesions.getSelectionModel().selectedIndexProperty(). addListener(  (o, oldVal, newVal) -> { 
+            hits = (listSesions.getSelectionModel().getSelectedItem()).getHits();
+            faults = (listSesions.getSelectionModel().getSelectedItem()).getFaults();
+            if (newVal.intValue() == -1) {
+                encerts.setText(Integer.toString(0));
+                fallades.setText(Integer.toString(0));
+            }
+            else{
+                
+            }
+             });
     }    
+
+    @FXML
+    private void tancarBoto(ActionEvent event) {
+    }
     
 }
